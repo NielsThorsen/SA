d6a01a71933e29c21c1f2a6ebc88530791403a73
HQMarius <mariusulrichvase@gmail.com>
2026-01-28 14:55:10 +0100

    123

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+class Queue
+{
+private:
+    int data;
+public:
+    Queue();
+    void Enqueue(int data);
+    int Dequeue();
+    bool full();
+    bool empty;
+    ~Queue();    
+};
+
+1::1(/* args */)
+{
+}
+
+1::~1()
+{
+}
--- a_queue.h
+++ a_queue.h
@@ -4,16 +4,16 @@ class AbstractQueue {
 public:
     
     // Tilføj et tal til køen
-    virtual void enqueue(int element) = 0;
+    virtual void enqueue(int data) = 0;
 
     // Fjern og returner det forreste tal
     virtual int dequeue() = 0;
 
     // Tjek om køen er tom
-    virtual bool isEmpty() = 0;
+    virtual bool Empty() = 0;
 
     // Tjek om køen er fuld
-    virtual bool isFull() = 0;
+    virtual bool Full() = 0;
 
     // Virtuel destructor 
     virtual ~AbstractQueue() {}
